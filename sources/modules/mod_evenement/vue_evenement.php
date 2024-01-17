e2e91c848b2c8fe389db6843ad97708e2485c091
aabdi <arsanbosss@gmail.com>
2024-01-17 13:48:29 +0100

    push

--- sources/modules/mod_evenement/vue_evenement.php
+++ sources/modules/mod_evenement/vue_evenement.php
@@ -17,14 +17,14 @@ class VueEvenement extends VueGenerique {
     function affichageDesEventsRetournes($type) {
         $cheminImages = '../sources/ressources/';
 
-        echo "<div class='container' style='margin-top: 300px;'>";
+        echo "<div class='container containerEvent'>";
 
         echo "<div class='row'>";
 
         foreach ($type as $event) {
 
             echo "<div class='col-md-4 col-sm-6'>";
-            echo "<div class='card mb-3'>";
+            echo "<div class='card mb-3 vignetteEvent'>";
 
             if (isset($event['logo']) && $event['logo'] != '') {
                 echo "<img class='card-img-top' src='" . htmlspecialchars($cheminImages . $event['logo']) . "' alt='Logo de l'événement'>";
