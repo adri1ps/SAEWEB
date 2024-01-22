f6bca1544375315f732ed60946a71e486f310ca0
IMMAN <i.amimman0.0@gmai.com>
2024-01-23 00:37:50 +0100

    Mise a jour page connexion

--- sources/Connexion.php
+++ sources/Connexion.php
@@ -1,27 +1,21 @@
 <?php
 
-
 class Connexion {
 
     protected static $bdd;
-
+    
     public static function initConnexion() {
+   
+        $dsn = "mysql:host=localhost;dbname=ButInfo";
+        $username = "ButInfo";
+        $password = "12345";
         
-        $dsn = 'mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201610';
-        $user = 'dutinfopw201610';
-        $mdp = 'tanehyhu';
-
         try {
-            
-            self :: $bdd = new PDO($dsn, $user, $mdp);
-        } catch (PDOException $e) {
-            
-            die('Erreur de connexion : ' . $e -> getMessage());
+            self::$bdd = new PDO($dsn, $username, $password);
+        }catch(PDOException $e) {
+            die("Error: ".$e->getMessage());
         }
     }
 }
-
-
-
 
 ?>
