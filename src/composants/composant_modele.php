3a1137ef09d2183b712e7035da14ed349de07759
arsanabdi <arsanbosss@gmail.com>
2024-01-25 23:38:32 +0100

    tout est bon pour profil et modif du profil

--- src/composants/composant_modele.php
+++
@@ -1,9 +0,0 @@
-<?php
-class CompMenuModele {
-    public function getMenuItems() {
-        // Ici, vous pouvez récupérer les éléments du menu depuis la source de données (base de données, tableau, etc.).
-        $menuItems = ['Joueurs', 'MonCompte'];
-        return $menuItems;
-    }
-}
-?>
