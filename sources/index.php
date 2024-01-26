3258afcb91b89be7bb83eb3d56b39d9834f6bc99
Rayankz <kherouarayan@gmail.com>
2024-01-26 15:27:40 +0100

    Projet final

--- sources/index.php
+++ sources/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css2/bootstrap.css">
     <link rel="stylesheet" href="style.css">
-    <title>Home Page</title>
+    <title>RED LINE DEFENSE</title>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
